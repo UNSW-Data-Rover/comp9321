1c6b751b141cb26e968757f20324830e18e7bb95
JCALiang <joexyzz@hotmail.com>
2018-05-23 05:58:55 +1000

    update server.py to allow cross domain access

--- DataPublicationAPI/server.py
+++ DataPublicationAPI/server.py
@@ -3,9 +3,10 @@ import json, csv, os, requests
 from bs4 import BeautifulSoup
 import urllib.request
 from pymongo import MongoClient
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/getallevents/', methods=['GET'])
 def getallevents():
